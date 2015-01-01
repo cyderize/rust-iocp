8b52f0e5a5588f8ca9d8ded72eab73916ce4c214
cyderize <admin@cyderize.org>
2015-01-01 16:47:00 +1100

    Add gh-pages
    Documentation added

---
+++ doc/implementors/core/kinds/trait.Sync.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iocp'] = ["<a class='stability Experimental' title='Experimental'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/kinds/trait.Sync.html' title='core::kinds::Sync'>Sync</a> for <a class='struct' href='iocp/struct.IoCompletionPort.html' title='iocp::IoCompletionPort'>IoCompletionPort</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
