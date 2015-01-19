567e0a82bce41518dd33fe4e25fda582fcabdb3d
cyderize <admin@cyderize.org>
2015-01-19 15:06:55 +1100

    Update docs for 0.0.5

---
+++ doc/implementors/core/marker/trait.Send.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iocp'] = ["<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='iocp/struct.IoCompletionPort.html' title='iocp::IoCompletionPort'>IoCompletionPort</a>","<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='enum' href='iocp/enum.IocpError.html' title='iocp::IocpError'>IocpError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/std/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iocp'] = ["<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.Error.html' title='std::error::Error'>Error</a> for <a class='enum' href='iocp/enum.IocpError.html' title='iocp::IocpError'>IocpError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
