bed387a598da905b64644665c4e3c94dfc3c6233
cyderize <admin@cyderize.org>
2015-01-29 13:58:26 +1100

    Update docs for 0.0.6

---
+++ doc/implementors/core/error/trait.Error.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['iocp'] = ["<a class='stability Unstable' title='Unstable: the exact API of this trait may change'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/error/trait.Error.html' title='core::error::Error'>Error</a> for <a class='enum' href='iocp/enum.IocpError.html' title='iocp::IocpError'>IocpError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
